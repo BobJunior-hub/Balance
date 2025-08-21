94c499d7d3726b46979583ee4e6973cdd09e248e
BobJunior-hub <bobirtoychiev03@gmail.com>
2025-08-21 09:23:52 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,45 @@
 let balance = parseInt(localStorage.getItem('balance')) || 0;
 const balanceEl = document.getElementById('balance')
-  const historyEl = document.getElementById('history') 
-    const amountInput = document.getElementById('amount')
-      balanceEl.textContent = balance;
-if (localStorage.getItem('history'))
-{ historyEl.innerHTML = localStorage.getItem('history')} 
-function changeBalance(type) { let amount = parseInt(amountInput.value)
-  if (isNaN(amount) || amount <= 0) { alert("Iltimos togri summa kiriting")
-    return } 
-  if (type === 'plus')
-  { balance += amount; addHistory(➕ <span class="in">+${amount}</span>) }
-  else { balance -= amount addHistory(➖ <span class="out">-${amount}</span>) }
-  balanceEl.textContent = balance; 
-    localStorage.setItem('balance', balance); amountInput.value = ""; }
-  function addHistory(text) { const div = document.createElement('div');
-      div.innerHTML = text; historyEl.prepend(div);
-      localStorage.setItem('history', historyEl.innerHTML); }
-  function clean() { balance = 0; 
-      balanceEl.textContent = balance; historyEl.innerHTML = ""; 
-      localStorage.setItem('balance', balance); localStorage.removeItem('history'); }
+const historyEl = document.getElementById('history')
+const amountInput = document.getElementById('amount')
+
+balanceEl.textContent = balance;
+
+if (localStorage.getItem('history')) {
+  historyEl.innerHTML = localStorage.getItem('history')
+}
+
+function changeBalance(type) {
+  let amount = parseInt(amountInput.value)
+  if (isNaN(amount) || amount <= 0) {
+    alert("Iltimos to‘g‘ri summa kiriting")
+    return
+  }
+
+  if (type === 'plus') {
+    balance += amount;
+    addHistory(`➕ <span class="in">+${amount}</span>`)
+  } else {
+    balance -= amount;
+    addHistory(`➖ <span class="out">-${amount}</span>`)
+  }
+
+  balanceEl.textContent = balance;
+  localStorage.setItem('balance', balance);
+  amountInput.value = "";
+}
+
+function addHistory(text) {
+  const div = document.createElement('div');
+  div.innerHTML = text;
+  historyEl.prepend(div);
+  localStorage.setItem('history', historyEl.innerHTML);
+}
+
+function clean() {
+  balance = 0;
+  balanceEl.textContent = balance;
+  historyEl.innerHTML = "";
+  localStorage.setItem('balance', balance);
+  localStorage.removeItem('history');
+}
